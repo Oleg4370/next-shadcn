2b63b023d0df31152201a3decd1bc4666c1f1c6f
Oleh Vls <oleh.vlasyik@gmail.com>
2026-02-13 17:32:56 +0200

    intro page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import { ThemeTogglerButton } from "@/components/theme-toggler-btn";
 import Image from "next/image";
+
+const LinkWrapper = ({
+  children,
+  link,
+}: {
+  children: React.ReactNode;
+  link: string;
+}) => (
+  <a
+    href={link}
+    className="font-medium text-zinc-950 dark:text-zinc-50 hover:text-zinc-400 dark:hover:text-zinc-600"
+  >
+    {children}
+  </a>
+);
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -13,52 +27,44 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mt-20">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            NextJS Basics
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Test website with some basic pages and forms built with{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://nextjs.org/docs"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Templates
+              Next.js 14
             </a>{" "}
-            or the{" "}
+            and{" "}
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://ui.shadcn.com/docs"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
-            </a>{" "}
-            center.
+              shadcn/ui
+            </a>
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <div>
+            <h2 className="text-xl font-semibold leading-10 tracking-tight">
+              Available Pages:
+            </h2>
+            <ul className="list-disc pl-5">
+              <li>
+                <LinkWrapper link="/login">Login page</LinkWrapper>
+              </li>
+              <li>
+                <LinkWrapper link="/password-reset">
+                  Password Reset page
+                </LinkWrapper>
+              </li>
+              <li>
+                <LinkWrapper link="/register">Registration page</LinkWrapper>
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
     </div>
