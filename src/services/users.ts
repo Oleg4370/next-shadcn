6f0384f38c6ce6eab2a9d0e09cb2b7634343338e
Oleh Vls <oleh.vlasyik@gmail.com>
2026-02-23 15:35:24 +0200

    fix database request

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,7 +1,14 @@
 import { DBType, UserType } from "@/types";
+import { headers } from "next/headers";
 
 export const getUsers = async (): Promise<UserType[]> => {
-  const data = await fetch("http://localhost:3000/db.json");
+  const headersList = await headers();
+  const host = headersList.get("x-forwarded-host") || headersList.get("host");
+  const protocol =
+    process.env.NODE_ENV === "production" ? "https://" : "http://";
+  const fullUrl = `${protocol}${host}`;
+
+  const data = await fetch(`${fullUrl}/db.json`);
   const res = (await data.json()) as DBType;
 
   return res.users;
